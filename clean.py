3edc196c725b0a1ee9d73ec352304f6fd512635e
weguerre-su <42718387+weguerre-su@users.noreply.github.com>
2020-10-27 15:15:02 -0400

    Create clean.py

---
+++ clean.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import xlrd
+df = pd.read_excel('Ey2.xlsx')
+
+df.rename(columns={'Unnamed: 0':'Airport','Unnamed: 1':'City','Unnamed: 2':'Month', 'Unnamed: 3':'CarrierID','Unnamed: 4':'Non_of_Flights' })
+
+df1 = df.dropna()
+
+df1
