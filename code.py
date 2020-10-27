cad4c44e3c4e1ffffd3d0552435a620038e8fad1
weguerre-su <42718387+weguerre-su@users.noreply.github.com>
2020-10-27 15:10:41 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,13 @@
 import pandas as pd
-df = pd.read_excel('path where excel file is stored/filename.xlsx')
-Print(df)
+import numpy as np
+import xlrd
+
+df = pd.read_excel('EY Case Study.xlsx')
+
+df.rename(columns={'Unnamed: 0':'Airport','Unnamed: 1':'City','Unnamed: 2':'Month', 'Unnamed: 3':'CarrierID','Unnamed: 4':'Non_of_Flights' })
+
+df1 = df.dropna()
+
+df1['Carrier'] = ('American')
+
+df1
